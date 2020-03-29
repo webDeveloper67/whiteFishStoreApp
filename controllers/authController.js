2845def853fa0d08363845a8848be4edbe46182f
mdee <design4web2014@gmail.com>
2020-03-30 00:57:11 +0430

    signup a user and generate the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,8 +12,35 @@ exports.signpUser = asyncMiddleware(async (req, res, next) => {
     password
   });
 
-  res.json(user);
+  sendTokenResponse(user, 200, res);
 });
+
+// Create token
+const createToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+// create cookie
+const sendTokenResponse = async (user, httpCode, res) => {
+  const token = createToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    )
+    // httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.status(httpCode).cookie('jwt', token, cookieOptions).json({
+    token
+  });
+};
 
 // Protect Auth Middleware
 exports.protect = asyncMiddleware(async (req, res, next) => {
