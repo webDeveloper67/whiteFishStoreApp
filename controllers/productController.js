d4d6d07d53d3ac1ac53f15d4891d2dcbdf1c0491
mdee <design4web2014@gmail.com>
2020-03-31 17:58:45 +0430

    product route along with all of itsfunctionality is not working

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
 const asyncMiddleware = require('./../helpers/asyncMiddleware');
 const ErrorResponse = require('./../helpers/ErrorResponse');
 const formidable = require('formidable');
 const Product = require('./../models/Product');
+const shopImage = './public/img/no-image-icon.png';
+const resolvedImg = path.resolve(shopImage);
 
 // CREATE a product
 exports.createProduct = asyncMiddleware((req, res, next) => {
@@ -42,6 +46,118 @@ exports.productByID = (req, res, next, id) => {
   });
 };
 
+// Get Single Product
+exports.getProduct = asyncMiddleware(async (req, res, next) => {
+  req.product.prodImg = undefined;
+  return res.json(req.product);
+});
+
+// Get the image of a product with defined image
+exports.photo = (req, res, next) => {
+  if (req.product.prodImg.data) {
+    res.set('Content-Type', req.product.prodImg.contentType);
+    return res.send(req.product.prodImg.data);
+  }
+  next();
+};
+
+// default PHOTO for products without defined Image
+exports.defaultPhoto = (req, res) => {
+  return res.sendFile(resolvedImg);
+};
+
+// Update a product
+exports.updateProduct = asyncMiddleware(async (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return next(new ErrorResponse('Photo could not be uploaded', 400));
+    }
+
+    let updatingProd = req.product;
+    updatingProd = _.extend(updatingProd, fields);
+    updatingProd.updated = Date.now();
+
+    if (files.prodImg) {
+      updatingProd.prodImg.data = fs.readFileSync(files.prodImg.path);
+      updatingProd.prodImg.contentType = files.prodImg.type;
+    }
+
+    const updatedProduct = await updatingProd.save();
+
+    return res.status(200).json(updatedProduct);
+  });
+});
+
+// List products via shopId
+exports.listProductByShop = asyncMiddleware(async (req, res, next) => {
+  const products = await Product.find({ shop: req.shop._id })
+    .populate('shop', '_id name')
+    .select('-image');
+
+  if (!products || products.length <= 0) {
+    return next(new ErrorResponse('Products not found!', 400));
+  }
+
+  res.json(products);
+});
+
+// List Latest Products
+exports.listLatestProducts = asyncMiddleware(async (req, res, next) => {
+  const products = await Product.find({})
+    .sort('-created')
+    .limit(5)
+    .populate('shop', '_id name');
+
+  if (!products || products.length <= 0) {
+    return next(new ErrorResponse('Latest products are not available!', 400));
+  }
+
+  res.json(products);
+});
+
+// List related Products
+exports.listRelatedProducts = asyncMiddleware(async (req, res, next) => {
+  const products = await Product.find({
+    _id: { $ne: req.product },
+    category: req.product.category
+  })
+    .limit(5)
+    .populate('shop', '_id name');
+
+  if (!products || products.length <= 0) {
+    return next(new ErrorResponse('Related Products can not be found!', 400));
+  }
+
+  res.json(products);
+});
+
+// List categories
+exports.listProductCategories = asyncMiddleware(async (req, res, next) => {
+  const products = await Product.distinct('category', {});
+
+  if (!products || products.length <= 0) {
+    return next(new ErrorResponse('categories can not be found!', 400));
+  }
+
+  res.json(products);
+});
+
+// Delete a Product
+exports.deleteProduct = asyncMiddleware(async (req, res, next) => {
+  let product = req.product;
+
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return next(new ErrorResponse('Product not found to delete', 400));
+    }
+    res.json(deletedProduct);
+  });
+});
+
+// Get all Products
 exports.getAllProducts = asyncMiddleware(async (req, res, next) => {
   const query = {};
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,9 @@ const productController = require('./../controllers/productController');
 const router = express.Router();
 
 router.route('/').get(productController.getAllProducts);
+router.route('/latest').get(productController.listLatestProducts);
+router.route('/categories').get(productController.listProductCategories);
+router.route('/defaultphoto').get(productController.defaultPhoto);
 
 router
   .route('/by/:shopId')
@@ -12,6 +15,26 @@ router
     authController.protect,
     shopController.isOwner,
     productController.createProduct
+  )
+  .get(productController.listProductByShop);
+
+router.route('/:productId').get(productController.getProduct);
+router.route('/related/:productId').get(productController.listRelatedProducts);
+router
+  .route('/image/:productId')
+  .get(productController.photo, productController.defaultPhoto);
+
+router
+  .route('/:shopId/:productId')
+  .put(
+    authController.protect,
+    shopController.isOwner,
+    productController.updateProduct
+  )
+  .delete(
+    authController.protect,
+    shopController.isOwner,
+    productController.deleteProduct
   );
 
 router.param('shopId', shopController.shopByID);
