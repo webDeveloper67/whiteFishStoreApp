00d4a95766aed2525e1f52e1624d788ad4c7ea8a
mdee <design4web2014@gmail.com>
2020-04-17 22:15:38 +0430

    prepare for deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 var cors = require('cors');
 const app = express();
@@ -40,5 +41,14 @@ app.all('*', async (req, res, next) => {
 
 // Error Controller Mounting
 app.use(errorController);
+
+// Serve Static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
